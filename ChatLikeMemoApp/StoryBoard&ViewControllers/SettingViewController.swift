f425d49f94ca05815091c843fcbe245b3636acb0
Naoyuki Umeda <naoyuki.umd@hotmail.co.jp>
2021-08-11 16:06:05 +0900

    Setting画面への遷移

--- ChatLikeMemoApp/StoryBoard&ViewControllers/SettingViewController.swift
+++ ChatLikeMemoApp/StoryBoard&ViewControllers/SettingViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class SettingViewController: UIViewController {
     
+    private let cellID = "settingCell"
+    
+    var settingList : [String] = ["ログアウト","お問い合わせ","バージョン"]
+    
     @IBAction func tappedReturnToChatList(_ sender: Any) {
         print("ボタン：tappedReturnToChatList")
         self.dismiss(animated: true, completion: nil)
     }
+    
+    @IBOutlet weak var settingTableView: UITableView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +27,40 @@ class SettingViewController: UIViewController {
         self.navigationController?.navigationBar.barTintColor = UIColor.purple
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.tintColor = .white
+        
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+        
+    }
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = settingTableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath) as! SettingTableViewCell
+        cell.settingCellLabel.text = settingList[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped table view")
     }
     
 }
 
+class SettingTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var settingCellLabel: UILabel!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
